411c5e4f1cb740382b392f892f9a149052fe3e5e
unknown <frodea111@gmail.com>
2024-05-26 18:05:42 +0600

    update user backend

---
+++ backend/app/Http/Controllers/UpdateController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UpdateController extends Controller
+{
+    public function getUser()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'users' => $users,
+        ]);
+    }
+    public function update_user(Request $request)
+    {
+        $user = User::find($request->id);
+
+        $notification = new Notification();
+
+        $notification->changer = $request->changer;
+        $notification->prev = $request->prev;
+        $notification->changed_to = $request->changed_to;
+
+        try {
+            $notification . save();
+            return response()->json([
+                'status' => 'success',
+                'msg' => 'Data Updated Successfully',
+                'code' => 200,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'msg' => $th->getMessage(),
+                'code' => 400,
+            ]);
+        }
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UpdateController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/get-user', [UpdateController::class, 'getUser']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/update_user', [UpdateController::class, 'update_user']);
